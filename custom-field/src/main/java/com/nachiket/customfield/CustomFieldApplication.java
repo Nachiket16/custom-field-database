30a653b7a811915e9c9304880d1307288eb9ba56
mintifi <nachiketkhule.nk@gmail.com>
2023-06-06 11:02:48 +0530

    first commit

---
+++ custom-field/src/main/java/com/nachiket/customfield/CustomFieldApplication.java
@@ -0,0 +1,13 @@
+package com.nachiket.customfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomFieldApplication.class, args);
+	}
+
+}
---
+++ custom-field/src/test/java/com/nachiket/customfield/CustomFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachiket.customfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
